4857e4c6a5391c5c134d35699217b2d7b8535d1d
milenaofarril <milena.ofarril@gmail.com>
2024-05-01 21:11:33 -0300

    classe Guiche

---
+++ src/Guiche.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guiche {
+
+    private boolean guicheLivre = true;
+    private int tempoOcupado = 0;
+
+    public boolean isGuicheLivre() {
+        return guicheLivre;
+    }
+
+    public void setGuicheLivre(boolean guicheLivre) {
+        this.guicheLivre = guicheLivre;
+    }
+
+    public int getTempoOcupado() {
+        return tempoOcupado;
+    }
+
+    public void setTempoOcupado(int tempoOcupado) {
+        this.tempoOcupado = tempoOcupado;
+    }
+
+    public List<Guiche> listaGuiche(){
+        ArrayList<Guiche> listaGuiche = new ArrayList<>(3);
+        for(int i = 0; i <2; i++){
+            listaGuiche.add(new Guiche());
+            listaGuiche.get(i);
+        }
+        return listaGuiche;
+    }
+
+    public boolean validaGuiche(List<Guiche> guiches){
+        for(int i = 0; i < guiches.size();i++){
+            if(!guiches.get(i).isGuicheLivre()){
+               setGuicheLivre(false);
+            }
+        }
+        return isGuicheLivre();
+    }
+}
--- src/Transacao.java
+++ src/Transacao.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.List;
 
 public class Transacao {
 
@@ -6,38 +6,44 @@ public class Transacao {
 
     private static int ATENDIMENTO = 21600;
     private int tempoTransacao;
-    private int saques, deposito, pagametos, tempo, tempoExtra;
+    private int saques, deposito, pagamentos, tempo, tempoExtra;
+    private Guiche guiche = new Guiche();
 
 
     public Transacao() {
         this.tempoTransacao = 0;
         this.deposito = 0;
-        this.pagametos = 0;
+        this.pagamentos = 0;
         this.saques = 0;
         this.tempo = 0;
         this.tempoExtra = 0;
     }
 
     public int trasacaoRealizada(int codigo){
+        List<Guiche> guicheLista = guiche.listaGuiche();
         while (tempo <= ATENDIMENTO){
 
             if (tempo >= ATENDIMENTO){
                 tempoExtra++;
             }
-            for(int i = 0; i <= 2; i++){
-                switch (codigo){
-                    case 0:
-                        tempoTransacao = tempo + 60;
-                        saques++;
-                        break;
-                    case 1:
-                        tempoTransacao = tempo + 90;
-                        deposito++;
-                        break;
-                    case 3:
-                        tempoTransacao = tempo + 120;
-                        pagametos++;
-                        break;
+            if(guiche.validaGuiche(guicheLista)){
+                for(int i = 0; i <= 2; i++){
+                    if(guicheLista.get(i).isGuicheLivre()){
+                        switch (codigo){
+                            case 0:
+                                tempoTransacao = tempo + 60;
+                                saques++;
+                                break;
+                            case 1:
+                                tempoTransacao = tempo + 90;
+                                deposito++;
+                                break;
+                            case 2:
+                                tempoTransacao = tempo + 120;
+                                pagamentos++;
+                                break;
+                        }
+                    }
                 }
             }
             tempo++;
